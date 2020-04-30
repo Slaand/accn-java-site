fb799ea38712e39592066901c5e9432e02e5af1a
Aleksandrs <owner@slaand.com>
2020-05-01 02:30:16 +0300

    Initial commit

---
+++ src/main/java/com/slaand/site/AccnJavaSiteApplication.java
@@ -0,0 +1,13 @@
+package com.slaand.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccnJavaSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccnJavaSiteApplication.class, args);
+	}
+
+}
